ce9983188ce03a7fdba7f79ac867bfa3b6bf361d
Jayden <administrator418@users.noreply.github.com>
2024-04-08 09:54:15 +0800

    1

--- FaceIdentify/path.py
+++ FaceIdentify/path.py
@@ -1,3 +1,3 @@
 cu_path = "/kaggle/working/FallDetection-FaceRecognition"
-casia_path = "/kaggle/input/CASIA-WebFace/CASIA-WebFace"
-lfw_path = "/kaggle/input/lfw/lfw"
+casia_path = "/kaggle/input/casia-webface/casia-webface"
+lfw_path = "/kaggle/input/dataset-lfw/lfw"
